7eb1b8ea004072194c497a59b5c6392d7f13366c
imac1 <irina.macovei@yahoo.fr>
2023-06-12 14:46:01 +0300

    up to level 4

--- Level2/level2.js
+++ Level2/level2.js
@@ -7,3 +7,21 @@
   `up`
   `down`
 */
+
+
+window.addEventListener("load", () => {
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp") {
+      moveDirection("up");
+    } else if (e.key === "ArrowDown") {
+      moveDirection("down");
+    } else if (e.key === "ArrowLeft") {
+      moveDirection("left");
+    } else if (e.key === "ArrowRight") {
+      moveDirection("right");
+    }
+  });
+
+ 
+});
--- Level3/level3.js
+++ Level3/level3.js
@@ -10,3 +10,33 @@
   writing multiple times the same function call.
 */
 
+window.addEventListener("load", () => {
+  // Code to run when the window has finished loading
+
+  document.addEventListener("keydown", (e) => {
+    const direction = getDirectionByKey(e.key);
+    if (direction) {
+      moveDirection(direction);
+    }
+  });
+
+  function getDirectionByKey(key) {
+    if (key === "ArrowUp") {
+      return "up";
+    } else if (key === "ArrowDown") {
+      return "down";
+    } else if (key === "ArrowLeft") {
+      return "left";
+    } else if (key === "ArrowRight") {
+      return "right";
+    }
+    return null; // Invalid key
+  }
+
+
+
+  const arrowDirections = ["right", "down", "left", "down", "right"];
+  for (let direction of arrowDirections) {
+    moveDirection(direction);
+  }
+});
